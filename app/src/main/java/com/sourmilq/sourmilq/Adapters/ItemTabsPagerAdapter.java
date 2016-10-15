22930b554274c15a579aeb332c4dc9c9d86d42f9
Philip Duncan <poduncan@uwaterloo.ca>
2016-10-15 17:29:23 -0400

    items fragment created on activity creation

--- app/src/main/java/com/sourmilq/sourmilq/Adapters/ItemTabsPagerAdapter.java
+++ app/src/main/java/com/sourmilq/sourmilq/Adapters/ItemTabsPagerAdapter.java
@@ -7,42 +7,39 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.sourmilq.sourmilq.Fragments.PantryItemsFragment;
 import com.sourmilq.sourmilq.Fragments.GroceryListItemsFragment;
 
+import java.util.ArrayList;
+
 /**
  * Created by Philip on 2016-10-15.
  */
 
 public class ItemTabsPagerAdapter extends FragmentPagerAdapter {
 
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> names = new ArrayList<>();
+
     public ItemTabsPagerAdapter(FragmentManager fm) {
         super(fm);
+
+        fragments.add(new GroceryListItemsFragment());
+        names.add("Grocery List"); // TODO make this a string resource
+
+        fragments.add(new PantryItemsFragment());
+        names.add("Pantry"); // TODO make this a string resource
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return fragments.size();
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return new GroceryListItemsFragment();
-            case 1:
-                return new PantryItemsFragment();
-            default:
-                return null;
-        }
+        return fragments.get(position);
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position) {
-            case 0:
-                return "Grocery list";
-            case 1:
-                return "Pantry";
-            default:
-                return null;
-        }
+        return names.get(position);
     }
 }
