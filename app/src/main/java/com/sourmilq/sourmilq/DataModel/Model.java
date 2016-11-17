3133f4f75ab5234202a87510cba9681064102418
Philip Duncan <poduncan@uwaterloo.ca>
2016-11-16 21:40:29 -0500

    persistent data initial (non-functional)

--- app/src/main/java/com/sourmilq/sourmilq/DataModel/Model.java
+++ app/src/main/java/com/sourmilq/sourmilq/DataModel/Model.java
@@ -1,5 +1,15 @@
 package com.sourmilq.sourmilq.DataModel;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -17,15 +27,47 @@ public class Model {
         pantryItems =  new ArrayList<>();
     }
 
-    public static Model getInstance() {
+    public static Model getInstance(Context context) {
         if(instance == null) {
             instance = new Model();
+            try {
+                FileInputStream fis = context.openFileInput(PersistentData.PERTISTENT_DATA_FILENAME);
+                ObjectInputStream is = new ObjectInputStream(fis);
+                PersistentData pData = (PersistentData) is.readObject();
+                is.close();
+                fis.close();
+                instance.setGroceryItems(pData.groceryItems);
+                instance.setPantryItems(pData.pantryItems);
+                // instance.setGroceryListId(pData.groceryListId);
+                // instance.setPantryListId(pData.pantryListId);
+                instance.setToken(pData.token);
+            } catch (IOException | ClassNotFoundException e) {
+                Toast.makeText(context, "Unable to open local persistent data", Toast.LENGTH_LONG)
+                        .show();
+            }
         }
         return instance;
     }
 
     public boolean hasValidToken(){
         return token != null;
+    }
+
+    public static void saveData(Context context) {
+        if (instance == null) return;
+
+        PersistentData pData = new PersistentData(instance);
+        try {
+            FileOutputStream fos = context.openFileOutput(
+                    PersistentData.PERTISTENT_DATA_FILENAME, Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(pData);
+            os.close();
+            fos.close();
+        } catch (IOException e) {
+            Toast.makeText(context, "Unable to save local persistent data", Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     public ArrayList<Item> getGroceryItems() {
---
+++ app/src/main/java/com/sourmilq/sourmilq/DataModel/PersistentData.java
@@ -0,0 +1,25 @@
+package com.sourmilq.sourmilq.DataModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Philip on 2016-11-16.
+ */
+
+public class PersistentData {
+    public static String PERTISTENT_DATA_FILENAME = "localdata.sourmilq";
+
+    public ArrayList<Item> groceryItems;
+    public ArrayList<Item> pantryItems;
+    public long groceryListId;
+    public long pantryListId;
+    public String token;
+
+    public PersistentData(Model m) {
+        groceryItems = m.getGroceryItems();
+        pantryItems = m.getPantryItems();
+        groceryListId = 0; // m.getGroceryListId();
+        pantryListId = 0; // m.getPantryListId();
+        token = m.getToken();
+    }
+}
